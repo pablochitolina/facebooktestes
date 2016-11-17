62772804104770e776aa7a4e2e7eea63f2d80ba0
Pablo Chitolina <pablochitolina@gmail.com>
2016-11-17 15:06:05 -0200

    v3

--- public/js/app.js
+++ public/js/app.js
@@ -8,13 +8,13 @@ var app = angular.module('starter', ['ngRoute', 'duScroll', 'angularModalService
 app.service('User', function () {
 
   var user = {
-    name : '',
-    lastName : '',
-    idUserFB : '',
-    email : '',
-    birthday : '',
-    gender : '',
-    picture : '/images/desconhecido.jpg'
+    name: '',
+    lastName: '',
+    idUserFB: '',
+    email: '',
+    birthday: '',
+    gender: '',
+    picture: '/images/desconhecido.jpg'
   };
 
   return {
@@ -40,7 +40,31 @@ app.config(function ($routeProvider) {
       templateUrl: '/templates/principal.html',
       controller: 'PrincipalCtrl'
     })
-    
+
     .otherwise({ redirectTo: '/principal' });
 
+});
+
+app.directive('googleAdsenseUm', function () {
+  return {
+    restrict: 'A',
+    replace: true,
+    template: '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8832799678197868" data-ad-slot="5421662634" data-ad-format="auto"></ins>',
+    controller: function () {
+      console.log("GoogleAdsenseUm: Google Adsense is being added!");
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  };
+});
+
+app.directive('googleAdsenseDois', function () {
+  return {
+    restrict: 'A',
+    replace: true,
+    template: '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8832799678197868" data-ad-slot="8375129036" data-ad-format="auto"></ins>',
+    controller: function () {
+      console.log("GoogleAdsenseDois: Google Adsense is being added!");
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  };
 });
